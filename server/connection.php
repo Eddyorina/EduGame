477b428af409b9a4677ff1df96c171a5364b2d99
John Waithaka <John.Waithaka@O365.strathmore.edu>
2018-06-08 12:28:39 +0300

    Added sign in functionality and single game for presentation purposes. To be corrected and improved

---
+++ server/connection.php
@@ -0,0 +1,15 @@
+<?php
+
+define("DB_HOST", "localhost");
+define("DB_USER", "root");
+define("DB_PASS", "");
+define("DB_DATABASE", "edugame");
+
+class Connect {
+
+	public function connectToDB() {
+		return new mysqli(DB_HOST, DB_USER, DB_PASS, DB_DATABASE);
+	}
+}
+
+?>
---
+++ server/login.php
@@ -0,0 +1,57 @@
+<?php
+
+class Login {
+
+	private $connect;
+
+	function __construct() {
+		require_once "connection.php";
+		$register = new Connect();
+		$this->connect = $register->connectToDB();
+	}
+
+	public function checkCredentials($email, $password){
+		$query = $this->connect->prepare("SELECT password FROM users WHERE email = ?");
+		$query->bind_param("s", $email);
+		if($query->execute()) {
+			$result = $query->get_result();
+			$num_of_rows = $result->num_rows;
+			if($num_of_rows > 0){
+				$user = $result->fetch_assoc();
+				if($user["password"] === $password){
+					$response["error"] = false;
+				} else {
+					$response["error"] = true;
+					$response["error_message"] = "Incorrect password";
+				}
+				echo json_encode($response);
+			} else {
+				$response["error"] = true;
+				$response["error_message"] = "Account does not exist";
+				echo json_encode($response);
+			}
+		} else {
+			echo "This is bad";
+		}
+	}
+
+	private function grantAccess($firstname){
+		echo "<h1>Welcome $firstname<h1>";
+	}
+
+}
+
+
+if(isset($_POST["email"]) && isset($_POST["password"])){
+	$username = $_POST["email"];
+	$password = $_POST["password"];
+	$login = new Login();
+
+	$login->checkCredentials($username, $password);
+} else {
+	$response["error"] = true;
+	$response["error_message"] = "Parameters not set";
+	echo json_encode($response);
+}
+
+?>
