be02c97799b25a6677ad9f547a92dca2bfe87892
johnGachihi <John.Waithaka@O365.strathmore.edu>
2018-06-24 13:15:15 +0300

    Added functionality to check if email is unique during sign up

--- server/email-check.php
+++ server/email-check.php
@@ -1,13 +1,16 @@
 <?php
 
+define("EMAIL_NOT_UNIQUE", 20);
+
 if(isset($_POST["email"])){
 	$email = $_POST["email"];
 
-	require_once "SignUp.php";
+	require_once "SignUpUtility.php";
 	$signup = new SignUp();
 
 	if(!$signup->emailUnique($email)){
 		$response["error"] = true;
+		$response["error_number"] = EMAIL_NOT_UNIQUE;
 		$response["error_message"] = "$email not unique";
 	} else {
 		$response["error"] = false;
